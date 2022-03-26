3377c85b0636cd9e883963c549ed028cbce76a5b
Mohammed-Mohra <101144856+Mohammed-Mohra@users.noreply.github.com>
2022-03-26 13:21:33 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Jan 24, 2022
+ *      Author: sa
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#define SYS_CLK_50MH		0
+#define SYS_CLK_84MH		1
+#define SYS_CLK_120MH		2
+
+
+
+#endif /* INC_MAIN_H_ */
